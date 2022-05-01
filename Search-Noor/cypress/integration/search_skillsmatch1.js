8034dd07ba0659f4f17fa6dfb2a1b703e892182d
Noor Kobari <68377272+noorkobari@users.noreply.github.com>
2022-05-02 02:03:24 +0300

    Update search_skillsmatch1.js

--- Search-Noor/cypress/integration/search_skillsmatch1.js
+++ Search-Noor/cypress/integration/search_skillsmatch1.js
@@ -2,13 +2,13 @@ describe('Search', () => {
   beforeEach(() => {  
     cy.visit('https://skillsmatch.mdx.ac.uk/en/search/')
   })
-
+    //log in by type these values in user name and password
     it('with all key words', () => {     
       cy.get('[id=username]').type('kev')
       cy.get('[id=password]').type('NnN82tpj9YR')
       cy.get('[value=Login]').click()
 
-      // enter two key words in the search bar
+      // enter two key words in the search bar then open the advanced options, choose the "With all of the keywords" and press on search button
       cy.get('[class=tagify__input]').type('planning {enter}')
       cy.get('[class=tagify__input]').type('management{enter}')
       cy.get('[test-data=AdvancedOptions]').click()
@@ -21,7 +21,7 @@ describe('Search', () => {
         .invoke('text')
         .then(text=>{
             var search1=text
-            cy.log(search1);
+            cy.log(search1); //Print a message to the Cypress Command Log that contains the first word in the search (work like return the value of it)
         })
 
       //get second word from search
@@ -29,10 +29,10 @@ describe('Search', () => {
         .invoke('text')
         .then(text=>{
             var search2=text
-            cy.log(search2);
+            cy.log(search2);//Print a message to the Cypress Command Log that contains the second word in the search (work like return the value of it)
         }) 
 
-        //compare the search word with result that it contains all the words from the seach
+        //check that the result coursers have both of the key words in it by comparing the texts
         cy.get("#search-result > div:nth-child(1) > span:nth-child(4)")
         .should('have.text','\n            planning\n        ');
         cy.get("#search-result > div:nth-child(1) > span:nth-child(5)")
